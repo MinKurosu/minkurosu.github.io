4ca1011d45d259997c4fa7573d8462f2bb1a807d
Min <yasmincruzm@hotmail.com>
2025-01-28 21:19:02 -0300

    odeioestamerda

--- feed.js
+++ feed.js
@@ -1,6 +1,9 @@
-jQuery(document).ready(function($) {
-  $(".fancybox").fancybox({
-      fitToView: false,
-      maxWidth: "80%"
-  });
-}); // ready
+$('.thumbs_small').fancybox({
+  openEffect: 'elastic',
+  closeEffect: 'elastic',
+  prevEffect: 'fade',
+  nextEffect: 'fade',
+  fitToView: false, // images won't be scaled to fit to browser's height
+  maxWidth: "90%" // images won't exceed the browser's width
+});
+
