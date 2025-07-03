6ea136a8233369ebc34710fe7ea573802aa23c7a
minkurosu <minkurosu@hotmail.com>
2025-07-03 18:15:38 -0300

    mapamine

--- script.js
+++ script.js
@@ -1,21 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
-    // --- Código para o efeito de loop de track ---
     const tracks = document.querySelectorAll('.track-inner');
 
     tracks.forEach(track => {
         // Duplica o conteúdo da track para criar o efeito de loop infinito
+        // Isso faz com que, quando a animação atinge o final da primeira cópia,
+        // ela já esteja começando a exibir a segunda cópia, criando a ilusão de loop.
         const content = track.innerHTML;
         track.innerHTML += content; // Adiciona o conteúdo original novamente
     });
-
-}
-
-  import { SkinViewer, WalkingAnimation } from 'skinview3d';
-
-// Seu código aqui
-const skinViewer = new SkinViewer({
-    canvas: document.getElementById("skin_container"),
-    width: 300,
-    height: 400,
-    skin: "C:\Users\yasmi\Desktop\meu-blog-firebase-novo\imgs\skins\me2020.png"
 });
