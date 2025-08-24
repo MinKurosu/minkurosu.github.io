ef0a3791dd8a6a6a3a1688442a8c184f998ece54
minkurosu <minkurosu@hotmail.com>
2025-08-24 17:47:08 -0300

    fml

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const tracks = document.querySelectorAll('.track-inner');
 
     tracks.forEach(track => {
-        // Duplica o conteúdo da track para criar o efeito de loop infinito
-        // Isso faz com que, quando a animação atinge o final da primeira cópia,
-        // ela já esteja começando a exibir a segunda cópia, criando a ilusão de loop.
+
         const content = track.innerHTML;
-        track.innerHTML += content; // Adiciona o conteúdo original novamente
+        track.innerHTML += content;
     });
 });
